1c60f6dc93dcc4bccb46b9b2c08a3b11aa69f423
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-05-03 11:16:24 +0900

    UISliderView & UITableView

--- UISlider Table/ViewController.swift
+++ UISlider Table/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var mySlider: UISlider!
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //mySlider.setValue(0.0, animated: true)
+        myTableView.dataSource = self
+        myTableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderMoved(_ sender: Any) {
+        print("slider value = \(Int(mySlider.value * 100))")
+        
+        // TableView의 값을 reload
+        myTableView.reloadData()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for : indexPath)
+        
+        let tableValue = String(Int(mySlider.value * 100))
+        cell.textLabel?.text = tableValue
+        //cell.textLabel?.text = String(Int(mySlider.value))
+        return cell
+    }
+    
+
 
 
 }
